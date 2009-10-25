12b335d6e5103451574e61ce8ce43f8b957f0fbc
oxlade39 <dan.oxlade@gmail.com>
2009-10-25 19:12:09 +0000

    adding more tests for jids

--- scampp.core.domain/src/main/scala/org/comperio/scampp/core/domain/JID.scala
+++ scampp.core.domain/src/main/scala/org/comperio/scampp/core/domain/JID.scala
@@ -1,23 +1,47 @@
 package org.comperio.scampp.core.domain
 
-object JID {
-
-  def apply(node : String, rest : String) = {
-    node + "@" + rest
+case class JID(n : String, d : String, r : String){
+  val node = {
+    if(n == null) null
+    else n.replace("@", "")
   }
 
-  def unapply(s: String): Option[(String, String)] = {
-    val parts = s split "@"
-    if(parts.length == 2) Some(parts(0), parts(1))
-    else None
+  val domain = d
+
+  val resource = {
+    if(r == null) null
+    else r.replace("/", "")
   }
 
+  override def toString = {
+    node + "@" + domain + "/" + resource
+  }
 
-  object JIDUtils {
+}
+
+object JIDExtractor {
+  val NODE_DOMAIN_RESOURCE_REGEXP = """(\w+@)?([\w+\d*\.]+\w+\d*)(/\w+)?""".r
+  val MAX_LENGTH = 3071
+
+  def apply(jid: JID) : String = {
+    jid.toString
+  }
+
+  def unapply(s: String): Option[JID] = {
+    s match {
+      case NODE_DOMAIN_RESOURCE_REGEXP(node, domain, resource) => Some(new JID(node, domain, resource))
+      case _ => None
+    }
+
+  }
+
+}
+
+object JIDUtils {
 
     class JIDStringWrapper(s : String) {
       def isJID = s match {
-        case JID(node, rest) => true
+        case JIDExtractor(JID(node, domain, resource)) => true
         case _ => false
       }
 
@@ -27,8 +51,5 @@ object JID {
     }
 
     implicit def stringWrapper(s : String) = new JIDStringWrapper(s)
-  }
 
 }
-
-
--- scampp.core.domain/src/test/scala/org/comperio/scampp/core/domain/JIDSpec.scala
+++ scampp.core.domain/src/test/scala/org/comperio/scampp/core/domain/JIDSpec.scala
@@ -7,7 +7,8 @@ import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 import scalatest.junit.JUnitRunner
 import org.junit.Assert.assertTrue
-import org.comperio.scampp.core.domain.JID.JIDUtils.stringWrapper
+import org.junit.Assert.assertFalse
+import org.comperio.scampp.core.domain.JIDUtils.stringWrapper
 
 /**
  * see http://www.ietf.org/rfc/rfc3920.txt
@@ -15,39 +16,62 @@ import org.comperio.scampp.core.domain.JID.JIDUtils.stringWrapper
 @RunWith(classOf[JUnitRunner])
 class JIDSpec extends FlatSpec with ShouldMatchers {
   val MAX_LENGTH = 3071
+  val JID_FORMAT = "[ node '@' ] JIDDomain [ '/' resource ]"
 
   "A JID" should "have a total maximum length of less than " + MAX_LENGTH + " bytes" in {
     assertTrue(
-      "dano@org.comperio/resource" isJID
+      "dano@org.comperio/resource" isAJID
     )
   }
 
   it should "not match string with total length greater than " +MAX_LENGTH+ " bytes" in {
     // FIXME this is incorrect, it generates a string of MAX_LENGTH characters not bytes!!!!!
-    val badString: String = RandomStringUtils.random(MAX_LENGTH + 1)
+    val badString = RandomStringUtils.random(MAX_LENGTH + 1)
+    assertFalse(
+      badString isAJID
+    )
+  }
+
+  it should "always have a domain section" in {
     assertTrue(
-      badString notAJID
+      "ietf.org" isAJID
+    )
+    assertTrue(
+      "org" isAJID
+    )
+    assertTrue(
+      "www.ietf.org" isAJID
+    )
+    assertTrue(
+      "dan@" notAJID
+    )
+    assertTrue(
+      "/dan" notAJID
     )
   }
                                           
-  it should "be of the following format: [ node '@' ] JIDDomain [ '/' resource ]" in {
+  it should "have an  optional node and '@' pair in the following format: "+JID_FORMAT+"" in {
     assertTrue(
-      "nodeNotAtSomething" notAJID
+      "dan@host.local" isAJID
+    )
+    assertTrue(
+      "host.local" isAJID
+    )
+    assertTrue(
+      "@host.local" notAJID
     )
   }
 
-}
-
-@RunWith(classOf[JUnitRunner])
-class JIDDomainSpec extends FlatSpec with ShouldMatchers {
-
-  "A JIDDomain" should "be either a JIDFullyQualifiedDomainName or a JIDAddressLiteral" is (pending)
-
-}
-
-@RunWith(classOf[JUnitRunner])
-class JIDAddressLiteralSpec extends FlatSpec with ShouldMatchers {
-
-  "A JIDDomain" should "be an IPv4address or an IPv6address" is (pending)
+  it should "have an  optional resource and '/' pair in the following format: "+JID_FORMAT+"" in {
+    assertTrue(
+      "host.local/printer" isAJID
+    )
+    assertTrue(
+      "host.local" isAJID
+    )
+    assertTrue(
+      "host.local/" notAJID
+    )
+  }
 
 }
