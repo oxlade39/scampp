6428dfb919c97de77cbbc0c29f0f4bf9ece5f577
unknown <dan.oxlade@.uk.easynet.corp>
2009-10-29 14:19:56 +0000

    updating scala lang and compiler and changing to Specs rather than scala test. Specs seems to work in both idea and maven, although maven reports twice as many tests as there are and says half of them were skipped.

--- scampp.core.domain/src/test/scala/org/comperio/scampp/core/domain/JIDSpec.scala
+++ scampp.core.domain/src/test/scala/org/comperio/scampp/core/domain/JIDSpec.scala
@@ -1,77 +1,76 @@
 package org.comperio.scampp.core.domain
 
-import apache.commons.lang.RandomStringUtils
+import org.apache.commons.lang.RandomStringUtils
 import java.lang.String
 import junit.runner.RunWith
-import org.scalatest.FlatSpec
-import org.scalatest.matchers.ShouldMatchers
-import scalatest.junit.JUnitRunner
+//import org.scalatest.junit.JUnitRunner
 import org.junit.Assert.assertTrue
 import org.junit.Assert.assertFalse
 import org.comperio.scampp.core.domain.JIDUtils.stringWrapper
+import org.specs._
+import org.specs.runner._
 
 /**
  * see http://www.ietf.org/rfc/rfc3920.txt
  */
-@RunWith(classOf[JUnitRunner])
-class JIDSpec extends FlatSpec with ShouldMatchers {
+class jidTest extends JUnit4(JIDSpec)
+
+object JIDSpec extends Specification {
   val MAX_LENGTH = 3071
   val JID_FORMAT = "[ node '@' ] JIDDomain [ '/' resource ]"
 
-  "A JID" should "have a total maximum length of less than " + MAX_LENGTH + " bytes" in {
-    assertTrue(
-      "dano@org.comperio/resource" isAJID
-    )
+  "A JID" should {
+    setSequential()
+    "A JID should have a total maximum length of less than " + MAX_LENGTH + " bytes" in {
+      assertTrue(
+        "dano@org.comperio/resource" isAJID
+        )
+    }
+    "should not match string with total length greater than " + MAX_LENGTH + " bytes" in {
+      // FIXME this is incorrect, it generates a string of MAX_LENGTH characters not bytes!!!!!
+      val badString = RandomStringUtils.random(MAX_LENGTH + 1)
+      assertFalse(
+        badString isAJID
+        )
+    }
+    "should always have a domain section" in {
+      assertTrue(
+        "ietf.org" isAJID
+        )
+      assertTrue(
+        "org" isAJID
+        )
+      assertTrue(
+        "www.ietf.org" isAJID
+        )
+      assertTrue(
+        "dan@" notAJID
+        )
+      assertTrue(
+        "/dan" notAJID
+        )
+    }
+    "should have an  optional node and '@' pair in the following format: " + JID_FORMAT + "" in {
+      assertTrue(
+        "dan@host.local" isAJID
+        )
+      assertTrue(
+        "host.local" isAJID
+        )
+      assertTrue(
+        "@host.local" notAJID
+        )
+    }
+    "should have an  optional resource and '/' pair in the following format: " + JID_FORMAT + "" in {
+      assertTrue(
+        "host.local/printer" isAJID
+        )
+      assertTrue(
+        "host.local" isAJID
+        )
+      assertTrue(
+        "host.local/" notAJID
+        )
+    }
   }
-
-  it should "not match string with total length greater than " +MAX_LENGTH+ " bytes" in {
-    // FIXME this is incorrect, it generates a string of MAX_LENGTH characters not bytes!!!!!
-    val badString = RandomStringUtils.random(MAX_LENGTH + 1)
-    assertFalse(
-      badString isAJID
-    )
-  }
-
-  it should "always have a domain section" in {
-    assertTrue(
-      "ietf.org" isAJID
-    )
-    assertTrue(
-      "org" isAJID
-    )
-    assertTrue(
-      "www.ietf.org" isAJID
-    )
-    assertTrue(
-      "dan@" notAJID
-    )
-    assertTrue(
-      "/dan" notAJID
-    )
-  }
-                                          
-  it should "have an  optional node and '@' pair in the following format: "+JID_FORMAT+"" in {
-    assertTrue(
-      "dan@host.local" isAJID
-    )
-    assertTrue(
-      "host.local" isAJID
-    )
-    assertTrue(
-      "@host.local" notAJID
-    )
-  }
-
-  it should "have an  optional resource and '/' pair in the following format: "+JID_FORMAT+"" in {
-    assertTrue(
-      "host.local/printer" isAJID
-    )
-    assertTrue(
-      "host.local" isAJID
-    )
-    assertTrue(
-      "host.local/" notAJID
-    )
-  }
-
 }
