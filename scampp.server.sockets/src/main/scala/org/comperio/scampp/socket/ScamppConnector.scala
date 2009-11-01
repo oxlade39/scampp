aa5e4fb3ca5f4a56647f6c74e89f05dbcbc94fc9
oxlade39 <dan.oxlade@gmail.com>
2009-11-01 15:27:08 +0000

    fixing up the actor and adding a new test

--- scampp.server.sockets/src/main/scala/org/comperio/scampp/socket/ScamppConnector.scala
+++ scampp.server.sockets/src/main/scala/org/comperio/scampp/socket/ScamppConnector.scala
@@ -1,29 +1,47 @@
 package org.comperio.scampp.socket
 
 
-import actors.Actor
 import actors.Actor._
-import java.net.ServerSocket
-
+import actors.Actor
+import java.net.{Socket, ServerSocket}
 abstract class ScamppConnector extends Actor{
-
   val port : Int
+  private var socketListeners : List[Actor] = List()
 
   def act() {
     val serverSocket = new ServerSocket(port)
     loop {
-      serverSocket.accept
+      val socket = serverSocket.accept
+      println("Connection established "+socket)
+      for(listener <- socketListeners) listener ! SocketConnected(socket)
 
-      react {
-        case Shutdown => exit("shutdown")
+      receiveWithin(0) {
+        case ScamppExit(m) => {
+          println("received ScamppExit")
+          exit(m)
+        }
+        case _ => {}
       }
     }
   }
 
+  def addConnectionListener(listener : Actor) {
+    socketListeners = listener :: socketListeners
+  }
+
+  def listen(body: => Unit) {
+    val actor = new Actor {
+      def act() = body
+    }
+    actor.start()
+    addConnectionListener(actor)
+  }
 }
 
 object DefaultScamppConnector extends ScamppConnector {
   val port = 5222
 }
 
-case class Shutdown
+abstract class ScamppEvent
+case class SocketConnected(socket : Socket) extends ScamppEvent
+case class ScamppExit(m : String) extends ScamppEvent
--- scampp.server.sockets/src/test/scala/org/comperio/scampp/socket/ScamppConnectorTest.scala
+++ scampp.server.sockets/src/test/scala/org/comperio/scampp/socket/ScamppConnectorTest.scala
@@ -1,6 +1,7 @@
 package org.comperio.scampp.socket
 
-import java.io.{OutputStream}
+import actors.Actor._
+import actors.{Actor}
 import java.net.Socket
 import org.specs._
 import org.specs.runner._
@@ -10,9 +11,9 @@ import org.junit.Assert._
 class ScamppConnectorTest extends JUnit4(ScamppConnectorSpec)
 object ScamppConnectorSpec extends Specification {
 
-  val scamppConnector = DefaultScamppConnector
+  var scamppConnector = DefaultScamppConnector
   doBeforeSpec { scamppConnector.start }
-  doAfterSpec { scamppConnector ! Shutdown }
+  doAfterSpec { sendDefaultKill }
 
   "A DefaultScamppConnector" should {
     "accept connections on port 5222" in {
@@ -20,7 +21,32 @@ object ScamppConnectorSpec extends Specification {
       val out = socket.getOutputStream()
       out.flush
       out.close
+      socket.isConnected() must be (true)
+    }
+    "publish a SocketConnected event when a new connection is established" in {
+      var notified = false
+
+      scamppConnector.listen {
+        react {
+          case SocketConnected(socket) => {
+            notified = true
+            socket.close
+            exit
+          }
+          case _ => exit
+        }
+      }
+
+      val socket = new Socket("localhost", 5222)
+      Thread.sleep(50L)
+      notified must be (true)
     }
   }
 
+  def sendDefaultKill() {
+    scamppConnector ! ScamppExit("test exit")
+    new Socket("localhost", 5222).close
+  }
+
 }
+
